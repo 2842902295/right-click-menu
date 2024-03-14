8cc9e458a9faf8acccdd083e75004877b042eb64
2842902295 <2842902295@qq.com>
2024-03-14 21:06:11 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -3,20 +3,53 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    {{'这是我的项目435452谢3谢'}}
-
-
-    {{'我去你大爷的'}}
-    <a href="https://vitejs.dev" target="_blank">
+  <div class="box" @contextmenu.prevent="showCustomMenu">
+    <a target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
-    <a href="https://vuejs.org/" target="_blank">
+    <a target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
+    <h1>任意位置右键展示效果</h1>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <custom-context-menu ref="customMenu" :menu-items="[{label: '我去'},{lable: '卧槽'}]"></custom-context-menu>
+  
 </template>
+
+<script>
+import CustomContextMenu from './components/CustomContextMenu.vue'
+export default {
+  name: 'HelloWorld',
+  components: {
+    CustomContextMenu, // 注册子组件
+  },
+  data() {
+    return {
+
+    };
+  },
+  methods: {
+    changeGreeting() {
+      alert('你好哇')
+    },
+    showCustomMenu(event) {
+      event.preventDefault(); // 阻止默认的右键菜单
+
+      // 计算自定义菜单的位置（基于鼠标位置）
+      const top = event.clientY;
+      const left = event.clientX;
+
+      console.log(this.$refs.customMenu)
+      // 假设你有一个自定义菜单的Vue组件ref为"customMenu"
+      this.$refs.customMenu.style.display = 'block'; // 显示自定义菜单
+      this.$refs.customMenu.style.top = top + 'px';
+      this.$refs.customMenu.style.left = left + 'px';
+
+      // 可能还需要添加一些逻辑来处理菜单项的点击事件等
+    },
+  }
+};
+</script>
 
 <style scoped>
 .logo {
@@ -25,10 +58,20 @@ import HelloWorld from './components/HelloWorld.vue'
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
+}
+
+.box {
+  position: fixed;
+  height: 100%;
+  width: 100%;
+  top: 0;
+  left: 0;
 }
 </style>
---
+++ src/components/CustomContextMenu.vue
@@ -0,0 +1,34 @@
+<!-- 自定义右键菜单组件 CustomContextMenu.vue -->
+<template>
+    <ul class="custom-context-menu" v-show="isVisible">
+        <li v-for="(item, index) in menuItems" :key="index" @click="handleMenuItemClick(item)">
+            {{ item.label }}
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    props: ['menuItems'],
+    data() {
+        return {
+            isVisible: true,
+        };
+    },
+    methods: {
+        handleMenuItemClick(item) {
+            this.isVisible = false; // 点击后隐藏菜单
+            // 执行对应的操作
+            item.action();
+        },
+    },
+    // ...
+};
+</script>
+
+<style scoped>
+.custom-context-menu {
+    position: fixed;
+    /* 添加样式以定位和样式化菜单 */
+}
+</style>
